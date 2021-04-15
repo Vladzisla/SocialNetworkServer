fac8aaee8fb22f7d9f10326d5ac0ac84f9ca8940
Vlad <vladis192@gmail.com>
2021-04-15 16:49:51 +0300

    add app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const usersRouter = require('./routes/users.routes')
+const cors = require('cors');
+
+const app = express()
+app.use(bodyParser.json());
+
+app.use(cors())
+
+app.use('/users', usersRouter)
+
+app.listen(3000, () => {
+    console.log('Server started at port:3000')
+})
